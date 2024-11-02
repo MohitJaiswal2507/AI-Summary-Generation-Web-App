02bb3279f747607e944a0a8e5d5aab4213e095d6
MohitJaiswal2507 <usernamechef2343@gmail.com>
2024-11-02 13:13:41 +0530

    token created

--- text-summary.py
+++ text-summary.py
@@ -2,7 +2,19 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 
-text = "The Mahabharata is an ancient Indian epic where the main story revolves around two branches of a family - the Pandavas and Kauravas - who, in the Kurukshetra War, battle for the throne of Hastinapura. Interwoven into this narrative are several smaller stories about people dead or living, and philosophical discourses."
+text = """The Mahabharata is an ancient Indian epic where 
+the main story revolves around two branches of a family - the Pandavas and Kauravas - who, in the Kurukshetra War,
+battle for the throne of Hastinapura. Interwoven into this narrative are several smaller stories about people dead or living, and philosophical discourses."""
 
 stopwords = list(STOP_WORDS)
-print(stopwords)
+# print(stopwords)
+
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(text)
+# print(doc)
+
+tokens = [token.text for token in doc]
+print(tokens)
+
+# punctuation = punctuation + '\n'
+# punctuation
