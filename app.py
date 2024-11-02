35ea3b13f77febbb7c0345c635860adeb05f11a7
MohitJaiswal2507 <usernamechef2343@gmail.com>
2024-11-02 15:25:11 +0530

    second commit

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import flask,render_template,request
 from text_summary import summarizer
+
+app = flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
