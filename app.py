6413587fe1c6951295cac541a36cd8290f959dd4
MohitJaiswal2507 <usernamechef2343@gmail.com>
2024-11-17 01:43:13 +0530

    prebuilt

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import flask,render_template,request
+from flask import Flask,render_template,request
 from text_summary import summarizer
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/analyze' , methods=['GET','POST'])
+def analyze():
+    if request.method == 'POST':
+        rawtext = request.form['rawtext']
+        summary, original_txt, len_org_txt , len_summary = summarizer(rawtext)
+        
+    return render_template('summary.html',summary=summary , original_txt = original_txt , len_org_txt=len_org_txt ,len_summary=len_summary )
 
 
 if __name__ == "__main__":
